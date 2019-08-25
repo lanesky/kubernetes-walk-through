53e6f0ea354ce8213e7750dca0c120aedcb4dff0
lanesky <shangkeyun@gmail.com>
2019-08-25 20:41:48 +0900

    squashed commits

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # kubernetes-walk-through
+
+
+## All steps
+
+* [Prerequisites](docs/01-prerequisites.md)
+* [Installing the HA Kubernete Cluster with kubeadm](docs/02-ha-kuberntes-cluster.md)
+* [Installing the Helm Tool](docs/03-installing-helm.md)
+* [Deploying the Prometheus and Grafana](docs/04-deploying-prometheus-and-grafana.md)
+* [Deploying the EFK(ElasticSearch, Fluentd, Kibana) and Setting up the log analysis](docs/05-deploying-EFK.md)
+* [Deploying the guestbook application with helm from the Jenkins pipeline](docs/06-deploying-app-with-jenkins-pipeline.md)
+* [Demonstrating the Blue/Green deployment](docs/07-blue-green-deployment.md)
+* [Demonstrating the Canary release](docs/08-canary-release.md)
+* [Cleaning Up](docs/09-cleaning-up.md)
---
+++ scripts/99-cleanup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+gcloud -q compute instances delete \
+  controller-0 controller-1 \
+  worker-0 worker-1 
+
+gcloud -q compute forwarding-rules delete kubernetes-forwarding-rule \
+  --region $(gcloud config get-value compute/region)
+
+gcloud -q compute target-pools delete kubernetes-target-pool
+
+gcloud -q compute http-health-checks delete kubernetes
+
+gcloud -q compute addresses delete mstakx-test
+
+gcloud -q compute firewall-rules delete \
+  mstakx-test-allow-nginx-service \
+  mstakx-test-allow-internal \
+  mstakx-test-allow-external \
+  mstakx-test-allow-health-check \
+  kubernetes-forwarding-rule
+
+gcloud -q compute firewall-rules delete mstakx-test-allow-health-check
+
+gcloud -q compute routes delete \
+  kubernetes-route-10-244-0-0-24 \
+  kubernetes-route-10-244-1-0-24 \
+  kubernetes-route-10-244-2-0-24
+
+
+
+gcloud -q compute networks subnets delete kubernetes
+
+gcloud -q compute networks delete mstakx-test
