aad91d4980c9264e999c8d1c33b9e9460f6e497d
lanesky <shangkeyun@gmail.com>
2019-08-25 22:57:17 +0900

    remove duplicated step

--- scripts/99-cleanup.sh
+++ scripts/99-cleanup.sh
@@ -17,17 +17,14 @@ gcloud -q compute firewall-rules delete \
   mstakx-test-allow-internal \
   mstakx-test-allow-external \
   mstakx-test-allow-health-check 
-
-  # \
+  
   # mstakx-test-allow-nginx-service \
-
-gcloud -q compute firewall-rules delete mstakx-test-allow-health-check
 
 gcloud -q compute routes delete \
   kubernetes-route-10-244-0-0-24 \
   kubernetes-route-10-244-1-0-24 
-
-  # \
+  
+  # uncomment if it has 3 workers
   # kubernetes-route-10-244-2-0-24
 
 
